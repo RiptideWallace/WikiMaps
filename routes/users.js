782c26aa0b5db19cf2bcb12389ffb88572f9e49b
Madeline Collier <madel@telus.net>
2017-07-20 22:10:45 +0000

    login now has a post route, which checkes email and password against DB

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,19 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  // router.get("/", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("users")
-  //     .then((results) => {
-  //       res.json(results);
-  //   });
-  // });
+  router.post("/login", (req, res) => {
+    knex
+      .select("*")
+      .from('users')
+      .where({email: req.body.email, password: req.body.password})
+      .then((results) => {
+        if (results.length === 0) {
+          res.status(404).send("This user is not in our DB");
+          return;
+        }
+        res.redirect("/");
+      });
+  });
   router.post("/register", (req, res) => {
     knex('users')
       .insert({name: req.body.username, email: req.body.email, password: req.body.password})

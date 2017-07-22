0b4b4ce9b2cf85ae3b7fb2d4d679dd2529756425
Madeline Collier <madel@telus.net>
2017-07-22 00:50:39 +0000

    scripts/user_profile now appends a user's fav maps to their profile, minor edit to user page html

--- public/scripts/user_profile.js
+++ public/scripts/user_profile.js
@@ -17,5 +17,21 @@ jQuery(document).ready(function(){
     // $(".userMaps").empty().text(error.responseText);
   });
 
+  $.ajax({
+    method: "GET",
+    url: "/users/" + userProfileId + "/favourites",
+  }).done((maps) => {
+    for(map of maps) {
+      ($("<div>").addClass("col-xs-12 col-sm-4")
+        .append($("<h2>").text(map.name))
+        .append($("<p>").text(map.description))
+        .append($("<img>").attr("src", map.image_url).addClass("img-responsive")))
+        .appendTo($("body").find(".fav-maps-container"));
+    }
+  }).fail((error) => {
+    console.log(error)
+    // $(".userMaps").empty().text(error.responseText);
+  });
+
 });
 
